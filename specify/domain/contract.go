97cc89fa6e2e6dcf426ccc43d0b4e5e66367a7e0
Kotaro Noguchi <kt.noguchi@gmail.com>
2022-05-25 23:08:32 +0900

    文字列のアサーションを含むシナリオを出力できる

--- specify/domain/contract.go
+++ specify/domain/contract.go
@@ -1,21 +1,27 @@
 package domain
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Contract struct {
-	Request Request `json:"request"`
+	Request  Request  `json:"request"`
 	Response Response `json:"response"`
 }
 
 type Request struct {
 	UrlPath string `json:"urlPath"`
-	Method string `json:"method"`
+	Method  string `json:"method"`
 }
 
 type Response struct {
-	Status int `json:"status"`
-	JsonBody interface{} `json:"jsonBody"`
+	Status   int  `json:"status"`
+	JsonBody JSON `json:"jsonBody"`
 }
+
+type JSON map[string]interface{}
+type JSONArray []interface{}
 
 func (c Contract) ToScenario() Scenario {
 	request := c.Request
@@ -32,8 +38,52 @@ func (c Contract) toSteps() []Step {
 	request := fmt.Sprintf("URL\"%s\"に%sリクエストを送る", c.Request.UrlPath, c.Request.Method)
 	statusCode := fmt.Sprintf("レスポンスステータスコードが\"%d\"である", c.Response.Status)
 
-	return []Step{
+	steps := []Step{
 		Step(request),
 		Step(statusCode),
 	}
+
+	return append(steps, c.Response.JsonBody.toAssertions([]string{})...)
+}
+
+type KeyChain []string
+
+func (k KeyChain) toPath() string {
+	return strings.Join(k, ".")
+}
+
+func (j JSON) toAssertions(keyChain KeyChain) []Step {
+	var assertions []Step
+	for k, v := range j {
+		keyChain := append(keyChain, k)
+		assertions = append(assertions, toAssertions(v, keyChain)...)
+	}
+	return assertions
+}
+
+func (j JSONArray) toAssertions(keyChain KeyChain) []Step {
+	var assertions []Step
+	for i, v := range j {
+		keyChain[len(keyChain) - 1] += fmt.Sprintf("[%d]", i)
+		assertions = append(assertions, toAssertions(v, keyChain)...)
+	}
+	return assertions
+}
+
+func toAssertions(v interface{}, keyChain KeyChain) []Step {
+	var steps []Step
+
+	switch v := v.(type) {
+	case map[string]interface{}:
+		steps = JSON(v).toAssertions(keyChain)
+	case []interface{}:
+		steps = JSONArray(v).toAssertions(keyChain)
+	case string:
+		step := fmt.Sprintf(`レスポンスのJSONの"$.%s"が文字列の"%s"である`, keyChain.toPath(), v)
+		steps = []Step{Step(step)}
+	default:
+		fmt.Printf("Warning: toAssertions - I don't know about type %T!\n", v)
+	}
+
+	return steps
 }
--- specify/domain/contract_test.go
+++ specify/domain/contract_test.go
@@ -14,7 +14,12 @@ var sut domain.Contract = domain.Contract{
 	},
 	domain.Response{
 		200,
-		nil,
+		domain.JSON{
+			"stringKey": "stringValue",
+			"objectKey": domain.JSON{
+				"stringKey": "objectStringValue",
+			},
+		},
 	},
 }
 
@@ -34,4 +39,16 @@ func TestToScenario_レスポンスステータスコード(t *testing.T) {
 	actual := sut.ToScenario()
 
 	assert.Contains(t, actual.Steps, domain.Step(`レスポンスステータスコードが"200"である`))
+}
+
+func TestToScenario_文字列のアサーション(t *testing.T) {
+	actual := sut.ToScenario()
+
+	assert.Contains(t, actual.Steps, domain.Step(`レスポンスのJSONの"$.stringKey"が文字列の"stringValue"である`))
+}
+
+func TestToScenario_オブジェクトに含まれる文字列のアサーション(t *testing.T) {
+	actual := sut.ToScenario()
+
+	assert.Contains(t, actual.Steps, domain.Step(`レスポンスのJSONの"$.objectKey.stringKey"が文字列の"objectStringValue"である`))
 }
