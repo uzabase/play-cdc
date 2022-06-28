40bd92a778998d0ee88e519dfd93bcdcbf33ac7d
Kotaro Noguchi <kt.noguchi@gmail.com>
2022-06-28 22:27:20 +0900

    this is for cdc

--- specify/repository/envRepository.go
+++ specify/repository/envRepository.go
@@ -15,9 +15,9 @@ func GetEnvs() []Env {
 	var result []Env
 
 	for i := 1; ; i++ {
-		apiEndpoint := os.Getenv(fmt.Sprintf("api_endpoint_%d", i))
-		apiName := os.Getenv(fmt.Sprintf("api_name_%d", i))
-		specPath := os.Getenv(fmt.Sprintf("spec_path_%d", i))
+		apiEndpoint := os.Getenv(fmt.Sprintf("cdc_api_endpoint_%d", i))
+		apiName := os.Getenv(fmt.Sprintf("cdc_api_name_%d", i))
+		specPath := os.Getenv(fmt.Sprintf("cdc_spec_path_%d", i))
 
 		if apiEndpoint == "" || apiName == "" || specPath == "" {
 			break
